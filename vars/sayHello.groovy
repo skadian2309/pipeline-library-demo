fe107cfdce9cf50d8e0c313b970af1a26e0bcd4b
skadian2309 <skadian1@gmail.com>
2020-05-10 22:11:36 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,12 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(Map config=[:]) {
-    echo  config.deploy
+def call(Map config=[:], Closure body ) {
+   // echo  config.deploy
     
     pipeline {
     agent {
          label 'maven'
     }
+                    body()
+
 }
+
 }
 
